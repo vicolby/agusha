51ff063042884cbf796d158bbeed85cdac51272f
Vi Colby <gimmefear@gmail.com>
2024-03-31 18:23:42 +0500

    feat: add get/del handlers

--- internal/handler/workspace_handler.go
+++ internal/handler/workspace_handler.go
@@ -16,6 +16,8 @@ func NewWorkspaceHandler(e *echo.Echo, service *services.WorkspaceService) {
 	handler := &WokspaceHandler{service: service}
 
 	e.POST("/workspace", handler.CreateWorkspace)
+    e.GET("/workspace", handler.GetAllWorkspaces)
+    e.DELETE("/workspace", handler.DeleteWorkspace)
 }
 
 func (h *WokspaceHandler) CreateWorkspace(c echo.Context) error {
@@ -28,6 +30,32 @@ func (h *WokspaceHandler) CreateWorkspace(c echo.Context) error {
 
 	if err != nil {
 		return echo.NewHTTPError(http.StatusNotFound, "Workspace creation failed")
+	}
+
+	return c.JSON(http.StatusOK, workspace)
+}
+
+func (h *WokspaceHandler) GetAllWorkspaces(c echo.Context) (error) {
+	workspaces, err := h.service.GetAllWorkspaces(c.Request().Context())
+
+	if err != nil {
+        echo.NewHTTPError(http.StatusNotFound, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, workspaces)
+}
+
+func (h *WokspaceHandler) DeleteWorkspace(c echo.Context) (error) {
+    workspace := &domain.Workspace{}
+
+	if err := c.Bind(workspace); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid workspace data")
+	}
+
+	err := h.service.DeleteWorkspace(c.Request().Context(), *workspace)
+
+	if err != nil {
+        echo.NewHTTPError(http.StatusNotFound, err.Error())
 	}
 
 	return c.JSON(http.StatusOK, workspace)
